ad4ee8e3fbc0abe203afe030061b88145cd2658d
gdesmar <gdesmar@cyber.gc.ca>
2021-10-28 14:03:04 +0000

    First iteration of the new elf module

---
+++ elf/al_elf.py
@@ -0,0 +1,229 @@
+import lief
+
+section_flags_entries = {entry.__int__(): entry for entry, txt in lief.ELF.SECTION_FLAGS.__entries.values()}
+segment_flags_entries = {entry.__int__(): entry for entry, txt in lief.ELF.SEGMENT_FLAGS.__entries.values()}
+
+
+def get_powers(x):
+    if x == 0:
+        return [0]
+    powers = []
+    i = 1
+    while i <= x:
+        if i & x:
+            powers.append(i)
+        i <<= 1
+    return powers
+
+
+def extract_fn(fn):
+    return {
+        "address": fn.address,
+        "name": fn.name,
+        "size": fn.size,
+        "value": fn.value,
+    }
+
+
+def extract_symbol(symbol):
+    symbol_struct = {
+        "binding": symbol.binding.name,
+        "name": symbol.name,
+        "type": symbol.type.name,
+        "exported": symbol.exported,
+        "imported": symbol.imported,
+        "visibility": symbol.visibility.name,
+    }
+    if symbol.name != symbol.demangled_name:
+        symbol_struct["demangled_name"] = symbol.demangled_name
+    return symbol_struct
+
+
+class AL_ELF:
+    def __init__(
+        self,
+        data=None,
+        binary=None,
+        extract_relocations=False,
+        extract_symbols=False,
+        extract_functions=False,
+    ):
+        if data is not None:
+            self.__dict__.update(data)
+            return
+
+        self.ctor_functions = [extract_fn(fn) for fn in binary.ctor_functions]
+        self.dtor_functions = [extract_fn(fn) for fn in binary.dtor_functions]
+
+        self.dynamic_entries = [
+            {
+                "tag": entry.tag.name,
+                "value": entry.value,
+            }
+            for entry in binary.dynamic_entries
+        ]
+
+        if extract_symbols:
+            self.dynamic_symbols = [extract_symbol(symbol) for symbol in binary.dynamic_symbols]
+            self.exported_symbols = [extract_symbol(symbol) for symbol in binary.exported_symbols]
+            self.imported_symbols = [extract_symbol(symbol) for symbol in binary.imported_symbols]
+            self.static_symbols = [extract_symbol(symbol) for symbol in binary.static_symbols]
+            # binary.symbols contains both static and dynamic symbols
+
+        if extract_functions:
+            self.exported_functions = [extract_fn(fn) for fn in binary.exported_functions]
+            self.functions = [extract_fn(fn) for fn in binary.functions]
+            self.imported_functions = [extract_fn(fn) for fn in binary.imported_functions]
+
+        if extract_relocations:
+            # TODO: Find one and work on it.
+            self.dynamic_relocations = [{"purpose": entry.purpose.name} for entry in binary.dynamic_relocations]
+            self.object_relocations = {}
+            self.pltgot_relocations = {}
+            self.relocations = {}
+
+        self.entrypoint = binary.entrypoint
+        self.format = binary.format.name
+        if binary.use_gnu_hash:
+            self.gnu_hash = {
+                "bloom_filters": binary.gnu_hash.bloom_filters,
+                "buckets": binary.gnu_hash.buckets,
+                "hash_values": binary.gnu_hash.hash_values,
+                "nb_buckets": binary.gnu_hash.nb_buckets,
+                "shift2": binary.gnu_hash.shift2,
+                "symbol_index": binary.gnu_hash.symbol_index,
+            }
+
+        if binary.has_interpreter:
+            self.interpreter = binary.interpreter
+
+        if binary.has_notes:
+            # TODO: find an ELF with notes, since details is very probably going to break.
+            self.notes = [
+                {
+                    "description": note.description,
+                    "details": note.details,
+                    "is_android": note.is_android,
+                    "is_core": note.is_core,
+                    "name": note.name,
+                    "type": note.type.name,
+                    "type_core": note.type_core.name,
+                }
+                for note in binary.notes
+            ]
+
+        self.nx = binary.has_nx
+
+        self.header = {
+            "arm_flags_list": [flag.name for flag in binary.header.arm_flags_list],
+            "entrypoint": binary.header.entrypoint,
+            "file_type": binary.header.file_type.name,
+            "header_size": binary.header.header_size,
+            "hexagon_flags_list": [flag.name for flag in binary.header.hexagon_flags_list],
+            "identity": binary.header.identity,
+            "identity_abi_version": binary.header.identity_abi_version,
+            "identity_class": binary.header.identity_class.name,
+            "identity_data": binary.header.identity_data.name,
+            "identity_os_abi": binary.header.identity_os_abi.name,
+            "identity_version": binary.header.identity_version.name,
+            "machine_type": binary.header.machine_type.name,
+            "mips_flags_list": [flag.name for flag in binary.header.mips_flags_list],
+            "numberof_sections": binary.header.numberof_sections,
+            "numberof_segments": binary.header.numberof_segments,
+            "object_file_version": binary.header.object_file_version.name,
+            "ppc64_flags_list": [flag.name for flag in binary.header.ppc64_flags_list],
+            "processor_flag": binary.header.processor_flag,
+            "program_header_offset": binary.header.program_header_offset,
+            "program_header_size": binary.header.program_header_size,
+            "section_header_offset": binary.header.section_header_offset,
+            "section_header_size": binary.header.section_header_size,
+            "section_name_table_idx": binary.header.section_name_table_idx,
+        }
+
+        self.imagebase = binary.imagebase
+
+        self.position_independent = binary.is_pie
+        self.last_offset_section = binary.last_offset_section
+        self.last_offset_segment = binary.last_offset_segment
+        self.libraries = binary.libraries
+        self.name = binary.name
+        self.next_virtual_address = binary.next_virtual_address
+        self.overlay = bytearray(binary.overlay).hex()
+        self.sections = [
+            {
+                "alignment": section.alignment,
+                # "content": section.content,
+                "entropy": section.entropy,
+                "entry_size": section.entry_size,
+                "file_offset": section.file_offset,
+                "flags": " | ".join([section_flags_entries[x].name for x in get_powers(section.flags.__int__())]),
+                "flags_list": [flag.name for flag in section.flags_list],
+                "information": section.information,
+                "link": section.link,
+                "name": section.name,
+                "name_idx": section.name_idx,
+                "offset": section.offset,
+                "original_size": section.original_size,
+                "segments": [segment.type.name for segment in section.segments],
+                "size": section.size,
+                "type": section.type.name,
+                "virtual_address": section.virtual_address,
+            }
+            for section in binary.sections
+        ]
+        self.segments = [
+            {
+                "alignment": segment.alignment,
+                # "content": segment.content,
+                "file_offset": segment.file_offset,
+                "flags": "".join([segment_flags_entries[x].name for x in get_powers(segment.flags.__int__())][::-1]),
+                "physical_address": segment.physical_address,
+                "physical_size": segment.physical_size,
+                "sections": [section.name for section in segment.sections],
+                "type": segment.type.name,
+                "virtual_address": segment.virtual_address,
+                "virtual_size": segment.virtual_size,
+            }
+            for segment in binary.segments
+        ]
+
+        self.strings = binary.strings
+
+        self.symbols_version = []
+        for sv in binary.symbols_version:
+            sv_struct = {
+                "value": sv.value,
+            }
+            if sv.has_auxiliary_version:
+                sv_struct["symbol_version_auxiliary"] = sv.symbol_version_auxiliary.name
+            self.symbols_version.append(sv_struct)
+
+        self.symbols_version_definition = [
+            {
+                "auxiliary_symbols": [{"name": aux_s.name} for aux_s in svd.auxiliary_symbols],
+                "flags": svd.flags,
+                "hash": svd.hash,
+                "version": svd.version,
+            }
+            for svd in binary.symbols_version_definition
+        ]
+
+        self.symbols_version_requirement = [
+            {
+                "auxiliary_symbols": [{"name": aux_s.name} for aux_s in svd.get_auxiliary_symbols()],
+                "name": svd.name,
+                "version": svd.version,
+            }
+            for svd in binary.symbols_version_definition
+        ]
+
+        if binary.use_sysv_hash:
+            # TODO: Verify why len(bucket) != nbucket and len(chains) != nchain
+            self.sysv_hash = {
+                "buckets": binary.sysv_hash.buckets,
+                "chains": binary.sysv_hash.chains,
+                "nbucket": binary.sysv_hash.nbucket,
+                "nchain": binary.sysv_hash.nchain,
+            }
+        self.type = binary.type.name
+        self.virtual_size = binary.virtual_size
---
+++ elf/elf.py
@@ -0,0 +1,168 @@
+import json
+import os
+
+import lief
+from assemblyline_v4_service.common.base import ServiceBase
+from assemblyline_v4_service.common.request import ServiceRequest
+from assemblyline_v4_service.common.result import Heuristic, Result, ResultSection
+
+import elf.al_elf
+
+
+class ELF(ServiceBase):
+    def __init__(self, config=None):
+        super(ELF, self).__init__(config)
+
+    def start(self):
+        self.log.info("Starting ELF")
+
+    def add_header(self):
+        res = ResultSection("Headers")
+        res.add_line(f"Entrypoint: {hex(self.elf.entrypoint)}")
+        res.add_line(f"Machine: {self.elf.header['machine_type']}")
+        res.add_line(f"File Type: {self.elf.header['file_type']}")
+        res.add_line(f"Identity Class: {self.elf.header['identity_class']}")
+        res.add_line(f"Endianness: {self.elf.header['identity_data']}")
+        res.add_line(f"Virtual Size: {self.elf.virtual_size}")
+        res.add_line(f"NX: {self.elf.nx}")
+        res.add_line(f"Position Independent: {self.elf.position_independent}")
+        res.add_line(f"Processor Flag: {self.elf.header['processor_flag']}")
+        if len(self.elf.header["arm_flags_list"]) > 0:
+            res.add_line(f"ARM Flags: {', '.join(self.elf.header['arm_flags_list'])}")
+        if len(self.elf.header["mips_flags_list"]) > 0:
+            res.add_line(f"MIPS Flags: {', '.join(self.elf.header['mips_flags_list'])}")
+        if len(self.elf.header["ppc64_flags_list"]) > 0:
+            res.add_line(f"PPC64 Flags: {', '.join(self.elf.header['ppc64_flags_list'])}")
+        if hasattr(self.elf, "interpreter"):
+            res.add_line(f"Interpreter: {self.elf.interpreter}")
+
+        overlay = bytes.fromhex(self.elf.overlay)
+        res.add_line(f"Overlay size: {len(overlay)}")
+        if len(overlay) > 0:
+            file_name = "overlay"
+            temp_path = os.path.join(self.working_directory, file_name)
+            with open(temp_path, "wb") as myfile:
+                myfile.write(overlay)
+            self.request.add_extracted(temp_path, file_name, f"{file_name} extracted from binary's resources")
+
+        self.file_res.add_section(res)
+
+    def add_sections(self):
+        if len(self.elf.sections) == 0:
+            return
+
+        res = ResultSection("Sections")
+        for section in self.elf.sections:
+            sub_res = ResultSection(f"Section - {section['name']}")
+            # TODO: have sections.name for elf
+            # sub_res.add_tag("file.elf.sections.name", section["name"])
+            sub_res.add_line(f"Type: {section['type']}")
+            sub_res.add_line(f"Entropy: {section['entropy']}")
+            if section["entropy"] > 7.5:
+                sub_res.set_heuristic(2)
+            sub_res.add_line(f"Size: {section['size']}")
+            sub_res.add_line(f"Flags: {', '.join(section['flags_list'])}")
+            if len(section["segments"]):
+                sub_res.add_line(f"Segments: {', '.join(section['segments'])}")
+            res.add_subsection(sub_res)
+        self.file_res.add_section(res)
+
+    def add_segments(self):
+        if len(self.elf.segments) == 0:
+            return
+
+        res = ResultSection("Segments")
+        for segment in self.elf.segments:
+            sub_res = ResultSection(f"Segment - {segment['type']}")
+            sub_res.add_line(f"Type: {segment['type']}")
+            sub_res.add_line(f"Flags: {segment['flags']}")
+            sub_res.add_line(f"Physical Size: {segment['physical_size']}")
+            sub_res.add_line(f"Virtual Size: {segment['virtual_size']}")
+            if len(segment["sections"]):
+                sub_res.add_line(f"Sections: {', '.join(segment['sections'])}")
+            res.add_subsection(sub_res)
+        self.file_res.add_section(res)
+
+    def add_libraries(self):
+        if len(self.elf.libraries) == 0:
+            return
+
+        res = ResultSection("Libraries")
+        res.add_lines(self.elf.libraries)
+        self.file_res.add_section(res)
+
+    def add_notes(self):
+        if not hasattr(self.elf, "notes"):
+            return
+        if len(self.elf.notes) == 0:
+            return
+
+        res = ResultSection("Notes")
+        for note in self.elf.notes:
+            sub_res = ResultSection(f"Note - {note['name']}")
+            sub_res.add_line(f"Description: {note['description']}")
+            sub_res.add_line(f"Type: {note['type']}")
+            if note["is_core"]:
+                sub_res.add_line(f"Core: {note['is_core']}, {note['type_core']}")
+            if note["is_android"]:
+                sub_res.add_line(f"Android: {note['is_android']}")
+            sub_res.add_line(f"Details: {note['details']}")
+            res.add_subsection(sub_res)
+        self.file_res.add_section(res)
+
+    def add_hash(self):
+        if hasattr(self.elf, "gnu_hash"):
+            res = ResultSection("GNU Hash")
+            res.add_line(f"Bloom Filters: {self.elf.gnu_hash['bloom_filters']}")
+            res.add_line(f"Buckets: {self.elf.gnu_hash['buckets']}")
+            res.add_line(f"Hash Values: {self.elf.gnu_hash['hash_values']}")
+            res.add_line(f"Number of buckets: {self.elf.gnu_hash['nb_buckets']}")
+            res.add_line(f"Shift2: {self.elf.gnu_hash['shift2']}")
+            res.add_line(f"Symbol Index: {self.elf.gnu_hash['symbol_index']}")
+            self.file_res.add_section(res)
+
+        if hasattr(self.elf, "sysv_hash"):
+            res = ResultSection("SYSV Hash")
+            res.add_line(f"Buckets: {self.elf.sysv_hash['buckets']}")
+            res.add_line(f"Chains: {self.elf.sysv_hash['chains']}")
+            res.add_line(f"Number of buckets: {self.elf.sysv_hash['nbucket']}")
+            res.add_line(f"Number of chains: {self.elf.sysv_hash['nchain']}")
+            self.file_res.add_section(res)
+
+    def add_symbols_version(self):
+        # TODO: Find and example that populates at least one of:
+        # symbols_version
+        # symbols_version_definition
+        # symbols_version_requirement
+        pass
+
+    def execute(self, request: ServiceRequest):
+        request.result = Result()
+        self.file_res = request.result
+        self.request = request
+
+        self.lief_binary = lief.parse(request.file_path)
+        if self.lief_binary is None:
+            res = ResultSection("This file looks like an ELF but failed loading.", heuristic=Heuristic(1))
+            self.file_res.add_section(res)
+            return
+
+        self.elf = elf.al_elf.AL_ELF(
+            binary=self.lief_binary,
+            extract_relocations=request.deep_scan,
+            extract_symbols=request.deep_scan,
+            extract_functions=request.deep_scan,
+        )
+
+        self.add_header()
+        self.add_sections()
+        self.add_segments()
+        self.add_libraries()
+        self.add_notes()
+        self.add_hash()
+        self.add_symbols_version()
+
+        temp_path = os.path.join(self.working_directory, "al_elf.json")
+        with open(temp_path, "w") as myfile:
+            myfile.write(json.dumps(self.elf.__dict__))
+        request.add_supplementary(temp_path, "al_elf.json", "Features extracted from the ELF file, as a JSON file")
